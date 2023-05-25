9118e4eea8d786a8cb5de6069bd88a098b65c5db
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-05-25 15:05:36 +0900

    app.tsxの編集に伴うコミット＆プッシュ

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {
+  ChakraProvider,
+  useDisclosure,
+  Box,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Portal,
+  Input,
+  Text,
+} from '@chakra-ui/react';
+
+import Icon from './conponent/icon';
+import { RepoModal } from './conponent/repoModal';
+import { TextInput } from './conponent/text';
+import { SearchButton } from './conponent/searchButton';
+import { SearchResult } from './conponent/searchResult';
+import theme from './theme';
+import { Method } from './method/method';
+import axios from 'axios';
 
 function App() {
+  const {
+    loading,
+    gitHubData,
+    textChange,
+    downEnter,
+    inputValue,
+    handleRequest,
+    handleData,
+    repoData,
+    isOpen,
+    onClose,
+    clickButtton,
+    filterTextChange,
+  } = Method();
+
+  //フィルター機能
+  //言語
+  //star
+  //issue数
+  //watcher
+  //fork
+  const {
+    onOpen: popoverOpen,
+    onClose: popoverClose,
+    isOpen: poopverOnOpne,
+  } = useDisclosure();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Box className="App">
+        <Box display={'flex'} justifyContent={'center'}>
+          <Box width={'80%'} display={'flex'} m={'5px'}>
+            <TextInput
+              inputValue={inputValue}
+              textChange={textChange}
+              downEnter={downEnter}
+            />
+            <SearchButton
+              handleRequest={handleRequest}
+              loading={loading}
+              inputValue={inputValue as string}
+            />
+          </Box>
+        </Box>
+
+        <Popover isOpen={poopverOnOpne} onOpen={popoverOpen}>
+          <PopoverTrigger>
+            <Button>フィルター</Button>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent>
+              <PopoverArrow />
+              <form onSubmit={clickButtton}>
+                <PopoverHeader>Header</PopoverHeader>
+                <PopoverBody>
+                  <Text>プロジェクト言語</Text>
+                  <Input
+                    onChange={(e) => {
+                      filterTextChange(e);
+                    }}
+                  />
+                  <Text>最低⭐️数</Text>
+                  <Input />
+                  <Text>最低👀数</Text>
+                  <Input />
+                  <Text>最低🍴数</Text>
+                  <Input />
+                  <Text>最高issue数</Text>
+                  <Input />
+                </PopoverBody>
+                <PopoverFooter>
+                  {' '}
+                  <Button
+                    type="submit"
+                    //  onClick={popoverClose}
+                  >
+                    Cancel
+                  </Button>
+                </PopoverFooter>
+              </form>
+            </PopoverContent>
+          </Portal>
+        </Popover>
+
+        <SearchResult gitHubData={gitHubData} handleData={handleData} />
+
+        {isOpen && (
+          <RepoModal isOpen={isOpen} onClose={onClose} repoData={repoData} />
+        )}
+      </Box>
+    </ChakraProvider>
   );
 }
 
